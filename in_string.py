374d8342dd6cefcaf410167feee5ea13bcdf83df
Juanola1206 <jinores@mail.austral.edu.ar>
2025-08-25 11:46:09 -0300

    Add files via upload

--- in_string.py
+++ in_string.py
@@ -1,19 +1,19 @@
 
-print("> Augusto")
-
-nombre = ("Augusto").lower() 
+nombre = input("Ingrese un nombre: ").lower()
+print(nombre)
+print(" ")
+print("Contiene a:", "a" in nombre)
+print("Contiene e:", "e" in nombre)
+print("Contiene i:", "i" in nombre)
+print("Contiene o:", "o" in nombre)
+print("Contiene u:", "u" in nombre)
+print("")
+print(nombre)
+print(" ")
+nombre = input("Ingrese otro nombre: ").lower()
 
 print("Contiene a:", "a" in nombre)
 print("Contiene e:", "e" in nombre)
 print("Contiene i:", "i" in nombre)
 print("Contiene o:", "o" in nombre)
 print("Contiene u:", "u" in nombre)
-
-print("> Ines")
-nombrer = ("Ines").lower() 
-
-print("Contiene a:", "a" in nombrer)
-print("Contiene e:", "e" in nombrer)
-print("Contiene i:", "i" in nombrer)
-print("Contiene o:", "o" in nombrer)
-print("Contiene u:", "u" in nombrer)
